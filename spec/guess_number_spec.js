f5847350ba19bdef369449bd9f99bb4090b0a0f2
bfzheng <baofeng_zheng@163.com>
2018-09-06 02:53:53 +0800

    finish print informatin according to user input

--- spec/guess_number_spec.js
+++ spec/guess_number_spec.js
@@ -1,14 +1,26 @@
 const guessNumber = require('../src/guess_number');
 
 describe('Guess Number', () => {
-    it('should return Wrong Input，Input again when input is [1 2]', function () {
-        let input = [1, 2];
-        expect(guessNumber(input)).toEqual("Wrong Input，Input again");
+    beforeEach(() => {
+        printMock = jasmine.createSpy();
+    });
+    it('should print Wrong Input，Input again when input is [1 2]', function () {
+        let input = ['1 2'];
+        guessNumber(input, printMock);
+        expect(printMock).toHaveBeenCalledWith("Wrong Input，Input again");
     });
 
-    it('should return Wrong Input，Input again when input contains repeat number', function () {
-        let input = [1, 1, 2, 3];
-        expect(guessNumber(input)).toEqual("Wrong Input，Input again");
+    it('should print Wrong Input，Input again when input contains repeat number', function () {
+        let input = ['1 1 2 3'];
+        guessNumber(input, printMock);
+        expect(printMock).toHaveBeenCalledWith("Wrong Input，Input again");
     });
+
+    it('should print Wrong Input，Input again when input contains repeat number', function () {
+        let input = ['1 5 6 7'];
+        guessNumber(input, printMock);
+        expect(printMock).toHaveBeenCalledWith("1 correct");
+    });
+
 
 });
--- src/guess_number.js
+++ src/guess_number.js
@@ -1,11 +1,51 @@
-const guessNumber = (input) => {
-    if (!inputValidation(input)) {
-        return "Wrong Input，Input again";
+const answer = [1, 2, 3, 4];
+
+const getResult = (inputElement, answer) => {
+    let numOfA = 0, numOfB = 0;
+    let res = {'Output': '', 'Instruction': ''};
+    let wrongPosition = [];
+    let correct = [];
+    for (let i = 0; i < answer.length; i++) {
+        if (answer[i] === inputElement[i]) {
+            numOfA++;
+            correct.push(answer[i]);
+        }
+        if (i !== inputElement.indexOf(answer[i])) {
+            numOfB++;
+            wrongPosition.push[answer[i]];
+        }
+    }
+    res.Output = numOfA + "A" + numOfB + "B";
+    if (numOfA === 4) {
+        res.Instruction = "win, all correct";
+        return res;
+    }
+    if (numOfB === 4) {
+        res.Instruction = "4 numbers position wrong";
+        return res;
+    }
+    if (numOfA === 0 && numOfB === 0) {
+        res.Instruction = "all wrong";
+        return res;
+    }
+
+    correct = correct.length === 0 ? "" : correct.join(" and ") + " correct";
+    wrongPosition = wrongPosition.length === 0 ? "" : wrongPosition.join(" and ") + " wrong position";
+    res.Instruction = (correct !== '' && wrongPosition !== '') ? correct + ", " + wrongPosition : correct + wrongPosition;
+    return res;
+}
+
+const guessNumber = (input, print) => {
+    for (let i = 0; i < input.length; i++) {
+        if (!inputValidation(input[i])) {
+            print("Wrong Input，Input again");
+        }
+        print(getResult(input[i].split(' ').map(value => parseInt(value)), answer).Instruction);
     }
 }
 
 const inputValidation = (input) => {
-    if (input.toString().length != 4 || new Set(input) < 4) {
+    if (input.split(' ').length != 4 || new Set(input.split(' ').map(value => parseInt(value))).size < 4) {
         return false;
     }
     return true;
